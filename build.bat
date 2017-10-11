798843dd41c8ae3a07da1a1a1a147b3fd478bec5
MachineMitch21 <mitchschutt@gmail.com>
2017-10-11 12:50:15 -0400

    Make with build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
-@echo off
 
 cd "./build"
+
+"C:/MinGW/bin/mingw32-make.exe"
+@echo off
+
 
 copy /y "libantisocial.dll"   "../../AntisocialTest/build/"
 copy /y "libantisocial.dll.a" "C:/MinGW/lib/"
