6fc61b1c09acf12ec59a9c2a7234797eddf97aaf
MachineMitch21 <mitchschutt@gmail.com>
2017-10-17 23:31:52 -0400

    Vector4f class

---
+++ src/Vector4f.cpp
@@ -0,0 +1,53 @@
+
+#include "Vector4f.h"
+
+using antisocial::math::Vector4f;
+
+Vector4f::Vector4f(float x, float y, float z, float w)
+    :   x(x),
+        y(y),
+        z(z),
+        w(w)
+{
+
+}
+
+Vector4f::~Vector4f()
+{
+
+}
+
+float Vector4f::magnitude()
+{
+    return sqrt((x * x) + (y * y) + (z * z) + (w * w));
+}
+
+float Vector4f::dot(const Vector4f& v)
+{
+    return (x * v.x) + (y * v.y) + (z * v.z) + (w * v.w);
+}
+
+Vector4f Vector4f::add(const Vector4f& v)
+{
+    return Vector4f(x + v.x, y + v.y, z + v.z, w + v.w);
+}
+
+Vector4f Vector4f::subtract(const Vector4f& v)
+{
+    return Vector4f(x - v.x, y - v.y, z - v.z, w - v.w);
+}
+
+float Vector4f::operator*(const Vector4f& v)
+{
+    return dot(v);
+}
+
+Vector4f Vector4f::operator+(const Vector4f& v)
+{
+    return add(v);
+}
+
+Vector4f Vector4f::operator-(const Vector4f& v)
+{
+    return subtract(v);
+}
---
+++ src/Vector4f.h
@@ -0,0 +1,47 @@
+
+#ifndef VECTOR4F_H
+#define VECTOR4F_H
+
+#include "AntisocialDLL.h"
+#include <math.h>
+
+namespace antisocial
+{
+    namespace math
+    {
+        class
+        #ifdef __WIN32__
+        ANTISOCIAL_API
+        #endif // __WIN32__
+        Vector4f
+        {
+        public:
+            Vector4f(float x, float y, float z, float w);
+            ~Vector4f();
+
+            float magnitude();
+            float dot(const Vector4f& v);
+            Vector4f add(const Vector4f& v);
+            Vector4f subtract(const Vector4f& v);
+
+            float operator*(const Vector4f& v);
+            Vector4f operator+(const Vector4f& v);
+            Vector4f operator-(const Vector4f& v);
+
+            friend std::ostream& operator<<(std::ostream& os, const Vector4f& v)
+            {
+                return os << "(" << v.x << ", " << v.y << ", " << v.z << ", " << v.w << ")";
+            }
+
+            std::string toString()
+            {
+                return std::string(std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(z) + ", " + std::to_string(w));
+            }
+
+        public:
+            float x, y, z, w;
+        };
+    }
+}
+
+#endif // VECTOR4F_H
